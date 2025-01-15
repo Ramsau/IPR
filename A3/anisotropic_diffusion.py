fee95e39d7df0c54a407668a6158a3590be4eb5d
Christoph Royer <mail@christophroyer.com>
2025-01-15 19:36:44 +0100

    Done until eq (7)

--- A3/anisotropic_diffusion.py
+++ A3/anisotropic_diffusion.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy.sparse as sp
 from scipy.ndimage import gaussian_filter
 from scipy.sparse.linalg import spsolve
+import scipy
 
 
 def diffusion_tensor(
@@ -18,6 +19,49 @@ def diffusion_tensor(
 ):
     # Implement the diffusion tensor (9)
     # Keep in mind which operations require a flattened image, and which don't
+
+    # m, n
+    u_tilde = gaussian_filter(u, sigma_u)
+    u_tilde_x = scipy.signal.convolve2d(u_tilde, np.array([[-1, 0, 1]]), mode="same")
+    u_tilde_y = scipy.signal.convolve2d(u_tilde, np.array([[-1], [0], [1]]), mode="same")
+
+    # m, n
+    S_xx = gaussian_filter(u_tilde_x * u_tilde_x, sigma_g)
+    S_yy = gaussian_filter(u_tilde_y * u_tilde_y, sigma_g)
+    S_xy = gaussian_filter(u_tilde_x * u_tilde_y, sigma_g)
+
+    # m, n, 2, 2
+    S = np.array([
+        [S_xx, S_xy],
+        [S_xy, S_yy],
+    ]).transpose(2, 3, 0, 1)
+    S_eig = np.linalg.eig(S)
+
+    # m*n
+    eig_vals = S_eig.eigenvalues.reshape(u.size, 2)
+    eig_vectors = S_eig.eigenvectors.reshape(u.size, 2, 2)
+    # resort
+    idx = eig_vals.argsort()
+    mu_1 = eig_vals[np.arange(eig_vals.shape[0]), idx[:, 1]]
+    mu_2 = eig_vals[np.arange(eig_vals.shape[0]), idx[:, 0]]
+    v_1 = eig_vectors[np.arange(eig_vectors.shape[0]), idx[:, 1]]
+    v_2 = eig_vectors[np.arange(eig_vectors.shape[0]), idx[:, 0]]
+
+    if mode == 'ced':
+        g = np.exp(np.pow(mu_1 - mu_2, 2) / (-2 * np.pow(gamma, 2)))
+        lambda_1 = alpha
+        lambda_2 = alpha + (1 - alpha) * (1 - g)
+    elif mode == 'eed':
+        lambda_1 = np.sqrt(1 + mu_1 / np.pow(gamma, 2)) # gamma=delta according to code template
+        lambda_2 = 1
+    else:
+        raise Exception("Unknown mode")
+
+    D_1 = lambda_1 * np.power(v_1[:, 0], 2) + lambda_2 * np.power(v_2[:, 0], 2)
+    D_2 = lambda_1 * np.power(v_1[:, 1], 2) + lambda_2 * np.power(v_2[:, 1], 2)
+    D_3 = lambda_1 * v_1[:, 0] * v_1[:, 1] + lambda_2 * v_2[:, 0] * v_2[:, 1]
+
+    # ok, but i can't create np.diag(D_1) - that would be 1.68 TiB...
     return sp.eye(2 * u.size)
 
 

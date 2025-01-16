87766cafee3acaef921940a58daf89b1a1c90090
Johannes Kattnig <johanneskattnig@gmx.at>
2025-01-16 15:13:33 +0100

    A3 finished

--- A3/anisotropic_diffusion.py
+++ A3/anisotropic_diffusion.py
@@ -20,60 +20,70 @@ def diffusion_tensor(
     # Implement the diffusion tensor (9)
     # Keep in mind which operations require a flattened image, and which don't
 
-    # m, n
     u_tilde = gaussian_filter(u, sigma_u)
-    u_tilde_x = scipy.signal.convolve2d(u_tilde, np.array([[-1, 0, 1]]), mode="same")
-    u_tilde_y = scipy.signal.convolve2d(u_tilde, np.array([[-1], [0], [1]]), mode="same")
+    u_deriv = nabla @ u_tilde.ravel()
+    u_tilde_x = u_deriv[:u_tilde.size].reshape(u.shape)
+    u_tilde_y = u_deriv[u_tilde.size:].reshape(u.shape)
 
-    # m, n
-    S_xx = gaussian_filter(u_tilde_x * u_tilde_x, sigma_g, axes=(0, 1))
-    S_yy = gaussian_filter(u_tilde_y * u_tilde_y, sigma_g, axes=(0, 1))
-    S_xy = gaussian_filter(u_tilde_x * u_tilde_y, sigma_g, axes=(0, 1))
+    S_xx = gaussian_filter(u_tilde_x * u_tilde_x, sigma_g)
+    S_yy = gaussian_filter(u_tilde_y * u_tilde_y, sigma_g)
+    S_xy = gaussian_filter(u_tilde_x * u_tilde_y, sigma_g)
 
-    # m, n, 2, 2
     S = np.array([
         [S_xx, S_xy],
         [S_xy, S_yy],
-    ]).transpose(2, 3, 0, 1)
-    S_eig = np.linalg.eig(S)
+    ])
+    eig_vals = np.zeros((u.shape[0], u.shape[1], 2))
+    eig_vecs = np.zeros((u.shape[0], u.shape[1], 2, 2))
 
-    # m*n
-    eig_vals = S_eig.eigenvalues.reshape(u.size, 2)
-    eig_vectors = S_eig.eigenvectors.reshape(u.size, 2, 2)
-    # resort
-    idx = eig_vals.argsort()
-    mu_1 = eig_vals[np.arange(eig_vals.shape[0]), idx[:, 1]]
-    mu_2 = eig_vals[np.arange(eig_vals.shape[0]), idx[:, 0]]
-    v_1 = eig_vectors[np.arange(eig_vectors.shape[0]), idx[:, 1]]
-    v_2 = eig_vectors[np.arange(eig_vectors.shape[0]), idx[:, 0]]
+    for i in range(u.shape[0]):
+        for j in range(u.shape[1]):
+            # Extract the 2x2 matrix for pixel (i, j)
+            S_ij = np.array([[S_xx[i, j], S_xy[i, j]], [S_xy[i, j], S_yy[i, j]]])
+            vals, vecs = np.linalg.eigh(S_ij)
+            # Sort eigenvalues in descending order
+            order = np.argsort(vals)[::-1]
+            eig_vals[i, j] = vals[order]
+            eig_vecs[i, j] = vecs[:, order]
 
+    mu_1 = eig_vals[:, :, 0]
+    mu_2 = eig_vals[:, :, 1]
+    v_1 = eig_vecs[:, :, :, 0]
+    v_2 = eig_vecs[:, :, :, 1]
+
+    # Compute λ1 and λ2 based on the mode
     if mode == 'ced':
-        g = np.exp(-np.pow(mu_1 - mu_2, 2) / (2 * np.pow(gamma, 2)))
+        g = np.exp(-((mu_1 - mu_2)**2) / (2 * gamma**2))
         lambda_1 = alpha
         lambda_2 = alpha + (1 - alpha) * (1 - g)
     elif mode == 'eed':
-        lambda_1 = np.pow(1 + mu_1 / np.pow(gamma, 2), -0.5) # gamma=delta according to code template
+        lambda_1 = (1 + mu_1 / gamma**2)**-0.5
         lambda_2 = 1
     else:
-        raise Exception("Unknown mode")
+        raise ValueError("Unknown mode. Use 'ced' or 'eed'.")
 
-    D_1 = lambda_1 * np.power(v_1[:, 0], 2) + lambda_2 * np.power(v_2[:, 0], 2)
-    D_2 = lambda_1 * np.power(v_1[:, 1], 2) + lambda_2 * np.power(v_2[:, 1], 2)
-    D_3 = lambda_1 * v_1[:, 0] * v_1[:, 1] + lambda_2 * v_2[:, 0] * v_2[:, 1]
+    D_1 = lambda_1 * (v_1[..., 0]**2) + lambda_2 * (v_2[..., 0]**2)
+    D_2 = lambda_1 * (v_1[..., 1]**2) + lambda_2 * (v_2[..., 1]**2)
+    D_3 = lambda_1 * (v_1[..., 0] * v_1[..., 1]) + lambda_2 * (v_2[..., 0] * v_2[..., 1])
 
-    # ok, but i can't create np.diag(D_1) - that would be 1.68 TiB...
-    D_U_1 = scipy.sparse.diags(D_1 * u.ravel())
-    D_U_2 = scipy.sparse.diags(D_2 * u.ravel())
-    D_U_3 = scipy.sparse.diags(D_3 * u.ravel())
+    diagD1 = sp.diags(D_1.ravel())
+    diagD2 = sp.diags(D_2.ravel())
+    diagD3 = sp.diags(D_3.ravel())
 
-    D_U = scipy.sparse.vstack([
-        scipy.sparse.hstack([D_U_1, D_U_3]),
-        scipy.sparse.hstack([D_U_3, D_U_2]),
-    ])
-
+    D_U = sp.bmat([
+        [diagD1, diagD3],
+        [diagD3, diagD2]
+    ], format='csr')
 
     return D_U
 
+def psnr(
+    x: np.ndarray,
+    y: np.ndarray,
+) -> float:
+    mse = np.mean((x - y) ** 2)
+    m = 1
+    return 10 * np.log10(m ** 2 / mse)
 
 def nonlinear_anisotropic_diffusion(
     image: np.ndarray,
@@ -133,3 +143,5 @@ if __name__ == "__main__":
     imageio.imwrite(
         f'./{mode}_out.png', (output.clip(0., 1.) * 255.).astype(np.uint8)
     )
+    reference_img = imageio.imread(mode + "_out_reference.png") / 255.
+    print(psnr(reference_img, output))
